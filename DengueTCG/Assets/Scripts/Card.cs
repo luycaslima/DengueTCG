9c18ba7182e7cd0c96cf447febe3eb4ae51c6f7e
LucasLima - Notebook <luycaslima@gmail.com>
2020-02-12 15:59:14 -0300

    Scriptable Object da Carta
    Criado a base pra gerar varias cartas e o display dos dados na imagem da carta

---
+++ DengueTCG/Assets/Scripts/Card.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Card" , menuName = "Card")]
+public class Card : ScriptableObject {
+    
+    //Cria um objeto que armazena os dados da carta
+    public new string title;
+    public int cost;
+    public Sprite artwork;
+    [TextArea]
+    public string description;
+    public int type; //tipo da carta 0 para ataque , 1 para defesa , 2 para situacional
+
+    public Sprite backgroundEffect; //O simbolo atras do valor de efeito muda de arcodor com tipo de carta?(ataque, defesa)
+    public int effect;
+
+
+}
---
+++ DengueTCG/Assets/Scripts/cardDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using TMPro;
+using UnityEngine.UI;
+
+public class cardDisplay : MonoBehaviour
+{
+    //Mostra os dados da carta na UI
+    public Card card;
+
+    public TextMeshProUGUI title;
+    public TextMeshProUGUI cost;
+    public TextMeshProUGUI description;
+    public TextMeshProUGUI effects;
+
+    public Image artwork;
+    public Image background; //simbolo do efeito da carta
+
+    //Pesquisar como receber os textos do textpro
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        title.text = card.title;
+        cost.text = card.cost.ToString();
+        description.text = card.description;
+        effects.text = card.effect.ToString();
+
+        //artwork.sprite = card.artwork;
+        //background.sprite = card.backgroundEffect;
+    }
+
+}
