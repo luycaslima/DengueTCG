452f77021189f0b97d94a67cf84cf80384fe9e6f
Lucas Desktop <luycaslima@gmail.com>
2020-02-27 02:58:24 -0300

    Pequenas mudanças no UI Manager

--- DengueTCG/Assets/Scripts/UIManager.cs
+++ DengueTCG/Assets/Scripts/UIManager.cs
@@ -12,12 +12,14 @@ public class UIManager : MonoBehaviour
 
     public void PauseButton()
     {
-
+        PauseMenu.DOAnchorPos(Vector2.zero, .30f);
+        BackGround.SetActive(true);
     }
 
     public void ResumeButton()
     {
-
+        PauseMenu.DOAnchorPos(new Vector2(0,600f), .30f);
+        BackGround.SetActive(false);
     }
 
 }
