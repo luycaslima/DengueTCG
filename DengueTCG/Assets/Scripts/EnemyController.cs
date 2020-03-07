f1b998c0cfdaa9611a45f239e9ad91a9e75be58f
LucasLima - Notebook <luycaslima@gmail.com>
2020-03-07 16:12:32 -0300

    Implementação do Status do Inimigo

--- DengueTCG/Assets/Scripts/EnemyController.cs
+++ DengueTCG/Assets/Scripts/EnemyController.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class EnemyController : MonoBehaviour
 {
+    public Enemy enemy;
+
+    [Header("Status Atual:")]
+    public int currentHp;
+    public int currentShield;
+
+    [Header("Referência dos Textos:")]
+    public Text nameText;
+    public Text hpText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nameText.text = enemy.name;
     }
 
     // Update is called once per frame
